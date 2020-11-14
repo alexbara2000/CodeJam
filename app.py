2792140a7b59dd44061a3374066c517bc024feb0
Alexandru Bara <alexandrubara2000@gmail.com>
2020-11-13 20:18:27 -0500

    app

---
+++ app.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+PATH = "C:\Users\alexa\Desktop\CodeJam"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.youtube.com/watch?v=Xjv1sY630Uc&ab_channel=TechWithTim")
