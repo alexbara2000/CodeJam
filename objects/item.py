ae94e37d676c5a8b9b3e7a49164effd847ef4002
Andreibr1 <andre.khaled.ibrahim@gmail.com>
2020-11-14 19:19:33 -0500

    fixed item.py

--- objects/item.py
+++ objects/item.py
@@ -6,23 +6,12 @@ import testGettingProce as scrapper
 
 class item:
     def __init__(self, url):
-        driver = getDriver('../chromedriver.exe')
-        driver.get(url)
+        data = scrapper.getData(url, "../chromedriver.exe")
         self.url =  url
-        self.name = scrapper.getname()
-        self.price = scrapper.getPrice()
+        self.name = data[1]
+        self.price = data[0]
     
     def printItem(self):
-        print(self.url + self.name + self.price)
+        print(self.url + self.name + str(self.price))
 
 
-
-#x = item("https://www.amazon.ca/TCL-50S425-CA-Ultra-Smart-Television/dp/B07KG318MQ/ref=sr_1_5?dchild=1&keywords=tv&qid=1605318869&sr=8-5")
-
-#print(x.price)
-
-#driver = getDriver('../chromedriver.exe')
-driver = getDriver('../chromedriver.exe')
-driver.get("https://www.amazon.ca/TCL-50S425-CA-Ultra-Smart-Television/dp/B07KG318MQ/ref=sr_1_5?dchild=1&keywords=tv&qid=1605318869&sr=8-5")
-
-print(scrapper.getname())
